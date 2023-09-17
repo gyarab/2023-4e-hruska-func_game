b2f18a4aea146d21fbde1962c805fa3ade2a6110
Filip Hruška <filip.hruska@student.gyarab.cz>
2023-09-17 20:26:13 +0200

    idk fixnout od základu

--- gaydb/src/views/GrafView.vue
+++ gaydb/src/views/GrafView.vue
@@ -2,8 +2,8 @@
   <h1 class="nadpis">This is a Graf page</h1>
   <form class="funcInput">
     <div id="app">
-      <input type="text" :value="myInput" placeholder="Function" id="textus" @input="get_value">
-      <button @click="submit" id="buttonus">Submit</button>
+      <input type="text" :value="myInput" placeholder="Function" id="textus">
+      <button @submit.prevent="submitform" id="buttonus">calculate</button>
     </div>
   </form>
   <div class="grafDiv">
@@ -20,21 +20,16 @@
     },
     mounted(){
       const canvas = document.querySelector(".graf");
-      const width = (canvas.width = window.innerWidth / 3);
-      const height = (canvas.height = window.innerHeight / 3);
-      const ctx = canvas.getContext("2d");
+      const width = (canvas.width = window.innerWidth / 2);
+      const height = (canvas.height = window.innerHeight / 2);
     },
     methods: {
-      submit(){
-
-      },
-      get_value(){
-        this.myInput = event.target.value
-        return this.myInput
+      submitform(){
+        this.draw_graph()
       },
       draw_graph(){
-        var func = this.get_value()
-        console.log(func)
+        var func = this.myInput
+        console.log(`the func: ${func}`)
         const ctx = canvas.getContext("2d");
         ctx.beginPath();
         ctx.moveTo(-500, eval(-500, func));
@@ -67,8 +62,8 @@
     text-align: center;
   }
   .funcInput{
-    padding: 10px;
-    margin: 10px;
+    padding: 0;
+    margin: 0;
     display: flex;
     justify-content: center;
     height: auto; 
