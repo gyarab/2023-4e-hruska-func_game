9018072cc9aff8113fa1efbe8afb9f232e77b98b
Filip Hruška <filip.hruska@student.gyarab.cz>
2023-09-23 21:28:12 +0200

    a

--- gaydb/src/views/GrafView.vue
+++ gaydb/src/views/GrafView.vue
@@ -24,7 +24,6 @@
       my_method(){
         const myCanvas = document.getElementById("graf"); //200 x 300 px
         const ctx = myCanvas.getContext("2d");
-        /*
         ctx.strokeStyle = "red";
         ctx.font = "15px Arial";
         ctx.fillText(this.function_input, 10, 10);
@@ -32,11 +31,12 @@
         ctx.rect(0,0,200,300)
         ctx.beginPath();
         ctx.moveTo(0, eval(0, this.function_input));
-        for (let i = 0; i < 500; i += 5){
+        for (let i = 0; i < 300; i += 1){
           ctx.lineTo(i, eval(i, this.function_input));
           ctx.moveTo(i, eval(i, this.function_input))
         }  
-        ctx.fill();
+        console.log(eval(300, this.function_input));
+        ctx.stroke();
       }
     }
   }
